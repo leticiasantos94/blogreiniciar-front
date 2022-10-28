154623a932f3a76cfe9af5bd0064a4c065d5d9c2
leticiasantos94 <leestudos94@gmail.com>
2022-10-28 12:21:44 -0300

    Criação do componente Home

---
+++ blog-reiniciar/src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+    <>
+    <h1 className="titulo">Reiniciar</h1>
+    <img src="https://observatorio3setor.org.br/wp-content/uploads/2019/09/1-girassol.jpg" 
+    alt="Campo de Girassol" className="img"/>
+    </>
+
+);
+}
+
+export default Home;
